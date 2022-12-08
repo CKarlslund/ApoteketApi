2018c35400370e4fe2e15dd5044dc12ab3868963
Cornelia Karlslund <cornelia.karlslund@nordkap.se>
2022-12-08 19:32:49 +0100

    Register OrderService in Program.cs

--- Apoteket/Apoteket.Api/Program.cs
+++ Apoteket/Apoteket.Api/Program.cs
@@ -1,6 +1,9 @@
+using Apoteket.Application.Orders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IOrderService, OrderService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
